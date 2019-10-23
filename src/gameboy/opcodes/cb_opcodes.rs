d7a36151d4f53901e5803a8333822012a63083e0
Guy Dunton <guydunton@economist.com>
2019-10-23 19:08:00 +0100

    Removing test opcode

--- src/gameboy/opcodes/cb_opcodes.rs
+++ src/gameboy/opcodes/cb_opcodes.rs
@@ -2,7 +2,6 @@ pub fn cb_code_to_opcode(code: u8) -> Result<&'static str, String> {
     match code {
         0x11 => Ok("RL C"),
         0x7C => Ok("BIT 7 H"),
-        0xAA => Ok("Thing"),
         _ => Err(format!("Unknown command 0xCB {:#X}", code)),
     }
 }
