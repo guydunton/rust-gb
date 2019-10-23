7fb80b9825b57cc499c318d0951ae4f1aa760a39
Guy Dunton <guydunton@economist.com>
2019-10-23 18:13:43 +0100

    Finish the CI script

--- scripts/update-supported.sh
+++ scripts/update-supported.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -eu
 
-cd ../docs
-
-SUPPORTED_CODES=$(cat ../src/gameboy/opcodes/opcodes.rs \
+SUPPORTED_CODES=$(cat src/gameboy/opcodes/opcodes.rs \
     | grep -E '0x[0-9A-F][0-9A-F] =>' \
     | sed 's/[[:space:]]*\(0x[0-9A-F][0-9A-F]\).*/"\1",/g')
 
@@ -12,22 +10,24 @@ SUPPORTED_CODES="${SUPPORTED_CODES%?}"
 # Put into a single line
 SUPPORTED_CODES=$(echo $SUPPORTED_CODES)
 
-CB_CODES=$(cat ../src/gameboy/opcodes/cb_opcodes.rs \
+CB_CODES=$(cat src/gameboy/opcodes/cb_opcodes.rs \
     | grep -E '0x[0-9A-F][0-9A-F] =>' \
     | sed 's/[[:space:]]*\(0x[0-9A-F][0-9A-F]\).*/"\1",/g')
 CB_CODES="${CB_CODES%?}"
 CB_CODES=$(echo $CB_CODES)
 
-sed "s/{supportedCodes}/${SUPPORTED_CODES}/g" src/SupportedCodes.elm.template \
+sed "s/{supportedCodes}/${SUPPORTED_CODES}/g" docs/src/SupportedCodes.elm.template \
 | sed "s/{supportedCBCodes}/${CB_CODES}/g" \
 | elm-format --stdin \
-> src/SupportedCodes.elm
+> docs/src/SupportedCodes.elm
 
 # Update git
 git update-index -q --refresh
 if ! git diff-index --quiet HEAD --; then
     # There are changes
-    echo "There are changes"
-    #git add docs/src/SupportedCodes.elm
-    #git commit -m "Automated commit of supported codes"
+    git add docs/src/SupportedCodes.elm
+    git commit -m "Automated commit of supported codes"
+    git remote rm origin
+    git remote add origin https://guydunton:<GITHUB_TOKEN>@github.com/guydunton/rust-gb.git
+    git push
 fi
--- src/gameboy/opcodes/cb_opcodes.rs
+++ src/gameboy/opcodes/cb_opcodes.rs
@@ -1,7 +1,7 @@
 pub fn cb_code_to_opcode(code: u8) -> Result<&'static str, String> {
     match code {
-            0x11 => Ok("RL C"),
-            0x7C => Ok("BIT 7 H"),
-            _ => Err(format!("Unknown command 0xCB {:#X}", code))
+        0x11 => Ok("RL C"),
+        0x7C => Ok("BIT 7 H"),
+        _ => Err(format!("Unknown command 0xCB {:#X}", code)),
     }
 }
