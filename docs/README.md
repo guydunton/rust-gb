8fd48e852c0acfb7a77456dc22e1a40a22ba7de1
Guy Dunton <guydunton@economist.com>
2019-10-23 21:34:43 +0100

    Clean up the CI and docs
    * Simplified the build script and removed the pre-build
    SupportedCodes.elm file from the project. This means that anyone wanting
    to run the code locally will need to run the script but makes the CI
    much simpler. The build script now doesn't need to commit to git in
    addition to github pages.
    * Added a readme to explain how to develop the docs locally.
    * Moved the build script back into the docs directory.

---
+++ docs/README.md
@@ -0,0 +1,10 @@
+# Rust GB Docs
+
+## Running the project locally
+
+1. cd into the docs directory `cd docs`
+2. Run update-supported.sh from the root of the project
+   `./update-supported.sh`
+3. Build the project: `elm make --output=js/elm.js src/Main.elm`
+4. Run using elm reactor: `elm reactor`
+5. Navigate to `localhost:8000` and view `index.html`
---
+++ docs/update-supported.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eux
+
+# This script just builds docs/src/SupportedCodes.elm.
+# Prerequisites:
+#   Run from the docs directory
+
+# Pull out all the opcodes from the file
+SUPPORTED_CODES=$(cat ../src/gameboy/opcodes/opcodes.rs \
+    | grep -E '0x[0-9A-F][0-9A-F] =>' \
+    | sed 's/[[:space:]]*\(0x[0-9A-F][0-9A-F]\).*/"\1",/g')
+
+# Remove the last ,
+SUPPORTED_CODES="${SUPPORTED_CODES%?}"
+
+# Put into a single line
+SUPPORTED_CODES=$(echo $SUPPORTED_CODES)
+
+# Repeat for the CB opcodes
+CB_CODES=$(cat ../src/gameboy/opcodes/cb_opcodes.rs \
+    | grep -E '0x[0-9A-F][0-9A-F] =>' \
+    | sed 's/[[:space:]]*\(0x[0-9A-F][0-9A-F]\).*/"\1",/g')
+CB_CODES="${CB_CODES%?}"
+CB_CODES=$(echo $CB_CODES)
+
+# Replace both parts in the template file and 
+# run through the elm formatter before saving into
+# the correct directory
+sed "s/{supportedCodes}/${SUPPORTED_CODES}/g" src/SupportedCodes.elm.template \
+| sed "s/{supportedCBCodes}/${CB_CODES}/g" \
+| elm-format --stdin \
+> src/SupportedCodes.elm
