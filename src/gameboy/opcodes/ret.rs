acb14ad6af195760144f26581c321be6ada0238b
Guy Dunton <guydunton@economist.com>
2019-10-28 08:48:46 +0000

    Fix the broken RET instruction
    * I changed the RET instruction to use the build in 16::from_be_bytes
    function as well as swapping the order of the bytes to be correct.

--- src/gameboy/opcodes/ret.rs
+++ src/gameboy/opcodes/ret.rs
@@ -11,16 +11,16 @@ impl OpCode {
         let stack_pointer = cpu.read_16_bits(RegisterLabel16::StackPointer);
 
         // Get the top 2 bytes of the stack
-        let top_byte = memory[stack_pointer as usize];
-        let next_byte = memory[stack_pointer as usize + 1];
+        let lower_byte = memory[stack_pointer as usize];
+        let higher_byte = memory[stack_pointer as usize + 1];
 
         // Move the stack pointer
         cpu.write_16_bits(RegisterLabel16::StackPointer, stack_pointer + 2);
 
         // Set the program counter to the value from the stack
-        let new_program_counter = ((top_byte as u16) << 8) & (next_byte as u16);
-        cpu.write_16_bits(RegisterLabel16::ProgramCounter, new_program_counter);
+        let new_program_counter = u16::from_be_bytes([higher_byte, lower_byte]);
 
+        cpu.write_16_bits(RegisterLabel16::ProgramCounter, new_program_counter);
         16
     }
 }
