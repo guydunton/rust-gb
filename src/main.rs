5084ade7428e7efeb19ab1fd4ae770c8d4d2ebd4
Guy Dunton <guydunton@economist.com>
2019-12-13 15:51:08 +0000

    Use piston_window instead of opengl directly

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,66 @@
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::{event_loop::*, input::*, window::WindowSettings};
+use piston_window::*;
 use std::env;
+
+extern crate image as img;
 
 mod debug_cli;
 mod gameboy;
 use crate::debug_cli::{update, DebugControls};
 use crate::gameboy::{Gameboy, ScreenColor, TickResult};
 
-fn screen_color_to_color(c: ScreenColor) -> [f32; 4] {
+fn screen_color_to_color(c: ScreenColor) -> [u8; 4] {
     match c {
-        ScreenColor::Black => [0.1, 0.2, 0.1, 1.0],
-        ScreenColor::Dark => [0.2, 0.4, 0.2, 1.0],
-        ScreenColor::Light => [0.5, 0.7, 0.1, 1.0],
-        ScreenColor::White => [0.6, 0.7, 0.1, 1.0],
+        ScreenColor::Black => [26, 51, 26, 255],
+        ScreenColor::Dark => [51, 102, 51, 255],
+        ScreenColor::Light => [128, 179, 26, 255],
+        ScreenColor::White => [153, 179, 26, 255],
     }
 }
 
 const SCREEN_WIDTH: u32 = 160;
 const SCREEN_HEIGHT: u32 = 144;
 
-fn render_screen(gb: &Gameboy, c: &graphics::Context, gl: &mut GlGraphics) {
-    use graphics::*;
-    let square = rectangle::square(0.0, 0.0, 2.0);
-
-    let screen_pixels = gb.get_screen_data();
-
-    for row in 0..SCREEN_HEIGHT {
-        for col in 0..SCREEN_WIDTH {
-            let pixel = screen_pixels[(col + row * SCREEN_WIDTH) as usize];
-            let color = screen_color_to_color(pixel);
-            let (x, y) = (col * 2, row * 2);
-
-            let transform = c.transform.trans(x as f64, y as f64);
-            rectangle(color, square, transform, gl);
-        }
-    }
-}
 
 pub struct App {
-    gl: GlGraphics,
+    texture_context: G2dTextureContext,
     gb: Gameboy,
     is_debug: bool,
     breakpoints: Vec<u16>,
 }
 
 impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
-        let gb = &self.gb;
+    fn render(&mut self, c: Context, g: &mut piston_window::G2d) {
+        let mut buffer: Vec<u8> = vec![0x00; (SCREEN_WIDTH * SCREEN_HEIGHT * 4) as usize];
 
-        self.gl.draw(args.viewport(), |c, gl| {
-            graphics::clear(WHITE, gl);
-            render_screen(&gb, &c, gl);
-        });
+        // Put the screen data into the buffer
+        self.gb.get_screen_data()
+            .iter()
+            .map(|col| screen_color_to_color(*col))
+            .enumerate()
+            .for_each(|(i, val)| {
+                let scaled = i * 4;
+                buffer[scaled] = val[0];
+                buffer[scaled+1] = val[1];
+                buffer[scaled+2] = val[2];
+                buffer[scaled+3] = val[3];
+            });
+
+        let canvas = 
+            img::ImageBuffer::from_vec(
+                SCREEN_WIDTH, 
+                SCREEN_HEIGHT,
+                buffer
+            ).unwrap();
+    
+        // Transform into a texture so piston can use it.
+        let texture: G2dTexture = Texture::from_image(
+            &mut self.texture_context,
+            &canvas,
+            &TextureSettings::new()
+        ).unwrap();
+
+        piston_window::image(&texture, c.transform.scale(2.0, 2.0), g);
     }
 
     fn update(&mut self, args: UpdateArgs) {
@@ -84,7 +90,6 @@ impl App {
 }
 
 fn main() {
-    let opengl = OpenGL::V3_2;
 
     let gb_screen_height = SCREEN_HEIGHT;
     let gb_screen_width = SCREEN_WIDTH;
@@ -92,9 +97,8 @@ fn main() {
     //let gb_screen_height = 256;
     //let gb_screen_width = 256;
 
-    let mut window: Window =
+    let mut window: PistonWindow =
         WindowSettings::new("Gameboy", [gb_screen_width * 2, gb_screen_height * 2])
-            .opengl(opengl)
             .exit_on_esc(true)
             .resizable(false)
             .build()
@@ -103,16 +107,19 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let mut app = App {
-        gl: GlGraphics::new(opengl),
+        texture_context:window.create_texture_context(),
         gb: Gameboy::new_with_bootloader(),
         is_debug: args.contains(&String::from("-d")),
         breakpoints: vec![],
     };
 
+
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
-        if let Some(r) = e.render_args() {
-            app.render(&r);
+        if let Some(_r) = e.render_args() {
+            window.draw_2d(&e, |context, g, _| {
+                app.render(context, g);
+            });
         }
 
         if let Some(u) = e.update_args() {
