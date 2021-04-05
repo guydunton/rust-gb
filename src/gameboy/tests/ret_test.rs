dfcc404ce69d294f7405a91803cadda9d97823a2
Guy Dunton <guydunton@economist.com>
2021-04-05 14:43:47 +0100

    ret test

--- src/gameboy/tests/ret_test.rs
+++ src/gameboy/tests/ret_test.rs
@@ -2,37 +2,30 @@
 mod ret_test {
     use crate::gameboy::Gameboy;
     use crate::gameboy::RegisterLabel16;
-    use rust_catch::tests;
 
-    tests! {
-        test("Ret jumps back to correct place") {
-            // 0x00, 0x01, 0x02
-            let mut gb = Gameboy::new(vec![0xC9, 0x34, 0x12]); // RET
+    #[test]
+    fn ret_jumps_back_to_correct_place() {
+        // 0x00, 0x01, 0x02
+        let mut gb = Gameboy::new(vec![0xC9, 0x34, 0x12]); // RET
 
-            // 0x00 : (0xC9) The RET instruction
-            // 0x01 : (0x34) The lower byte of the return address
-            // 0x02 : (0x12) The higher byte of the return address
+        // 0x00 : (0xC9) The RET instruction
+        // 0x01 : (0x34) The lower byte of the return address
+        // 0x02 : (0x12) The higher byte of the return address
 
-            // The stack pointer points to 0x01 which means the value
-            // at the top of the stack is 0x1234
-            gb.set_register_16(RegisterLabel16::StackPointer, 0x01);
+        // The stack pointer points to 0x01 which means the value
+        // at the top of the stack is 0x1234
+        gb.set_register_16(RegisterLabel16::StackPointer, 0x01);
 
-            section("RET takes 16 cycles") {
-                let cycles = gb.step_once();
-                assert_eq!(cycles, 16);
-            }
+        let cycles = gb.step_once();
 
-            section("RET instruction works") {
-                let _ = gb.step_once();
+        // RET takes 16 cycles
+        assert_eq!(cycles, 16);
 
-                // Program counter is now in the correct place
-                assert_eq!(gb.get_register_16(RegisterLabel16::ProgramCounter), 0x1234);
+        // Program counter is now in the correct place
+        assert_eq!(gb.get_register_16(RegisterLabel16::ProgramCounter), 0x1234);
 
-                // Stack pointer is also in the right place
-                assert_eq!(gb.get_register_16(RegisterLabel16::StackPointer), 0x03);
-            }
-
-        }
+        // Stack pointer is also in the right place
+        assert_eq!(gb.get_register_16(RegisterLabel16::StackPointer), 0x03);
     }
 
     #[test]
