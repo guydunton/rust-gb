c0c1a3f5ba03726a9e99f3300a4ae548fa030cd5
Guy Dunton <guydunton@economist.com>
2022-02-08 13:32:03 +0000

    Implement most of or

--- src/gameboy/tests/or.rs
+++ src/gameboy/tests/or.rs
@@ -2,7 +2,14 @@ use crate::gameboy::{Flags, Gameboy, RegisterLabel16, RegisterLabel8};
 
 #[test]
 fn or_r8_test() {
-    let opcodes = vec![(0xB1, RegisterLabel8::C)];
+    let opcodes = vec![
+        (0xB0, RegisterLabel8::B),
+        (0xB1, RegisterLabel8::C),
+        (0xB2, RegisterLabel8::D),
+        (0xB3, RegisterLabel8::E),
+        (0xB4, RegisterLabel8::H),
+        (0xB5, RegisterLabel8::L),
+    ];
 
     for (opcode, register) in &opcodes {
         let mut gb = Gameboy::new(vec![*opcode]);
@@ -32,4 +39,23 @@ fn or_r8_test() {
 
         assert_eq!(gb.get_flag(Flags::Z), true);
     }
+}
+
+#[test]
+fn writing_to_a_works() {
+    let mut gb = Gameboy::new(vec![0xB7, 0xB7]);
+
+    gb.set_register_8(RegisterLabel8::A, 0);
+
+    gb.step_once();
+
+    assert_eq!(gb.get_flag(Flags::Z), true);
+    assert_eq!(gb.get_register_8(RegisterLabel8::A), 0);
+
+    // Check the reverse
+    gb.set_register_8(RegisterLabel8::A, 1);
+    gb.step_once();
+
+    assert_eq!(gb.get_flag(Flags::Z), false);
+    assert_eq!(gb.get_register_8(RegisterLabel8::A), 1);
 }
