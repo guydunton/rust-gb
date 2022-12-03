f515cc49be5730f89a5bbd72c76b48b31394f1c7
Guy Dunton <guydunton@economist.com>
2022-12-03 21:27:58 +0000

    Add conditions to ret instruction (#29)
    * Add conditions to the ret instruction
    * Make the jump conditions public to allow their use in tests
    * Add the RET NZ instruction

--- src/gameboy/opcodes/run_fns/ret.rs
+++ src/gameboy/opcodes/run_fns/ret.rs
@@ -1,8 +1,33 @@
 use crate::gameboy::cpu::CPU;
 
-use crate::gameboy::RegisterLabel16;
+use crate::gameboy::opcodes::{Argument, JumpCondition};
+use crate::gameboy::{read_flag, Flags, RegisterLabel16};
 
-pub fn run_ret(cpu: &mut CPU, memory: &mut [u8]) -> u32 {
+pub fn run_ret(args: &[Argument], cpu: &mut CPU, memory: &mut [u8]) -> u32 {
+    // If there is a condition then check it
+
+    let mut extra_cycles = 0;
+
+    let should_return = match args[0] {
+        Argument::JumpCondition(condition) => {
+            extra_cycles += 4;
+            match condition {
+                JumpCondition::NotZero => !read_flag(cpu, Flags::Z),
+                JumpCondition::Zero => read_flag(cpu, Flags::Z),
+            }
+        }
+        _ => true,
+    };
+
+    if should_return {
+        perform_return(cpu, memory);
+        return 16 + extra_cycles;
+    } else {
+        return 8;
+    }
+}
+
+fn perform_return(cpu: &mut CPU, memory: &mut [u8]) {
     let stack_pointer = cpu.read_16_bits(RegisterLabel16::StackPointer);
 
     // Get the top 2 bytes of the stack
@@ -16,5 +41,4 @@ pub fn run_ret(cpu: &mut CPU, memory: &mut [u8]) -> u32 {
     let new_program_counter = u16::from_be_bytes([higher_byte, lower_byte]);
 
     cpu.write_16_bits(RegisterLabel16::ProgramCounter, new_program_counter);
-    16
 }
--- src/gameboy/tests/ret_test.rs
+++ src/gameboy/tests/ret_test.rs
@@ -1,41 +1,106 @@
-#[cfg(test)]
-mod ret_test {
-    use crate::gameboy::Gameboy;
-    use crate::gameboy::RegisterLabel16;
+use crate::gameboy::cpu::CPU;
+use crate::gameboy::memory_adapter::MemoryAdapter;
+use crate::gameboy::opcodes::Argument;
+use crate::gameboy::opcodes::Category;
+use crate::gameboy::opcodes::JumpCondition;
+use crate::gameboy::tests::decode_util::decode;
+use crate::gameboy::write_flag;
+use crate::gameboy::Flags;
+use crate::gameboy::Gameboy;
+use crate::gameboy::OpCode;
+use crate::gameboy::RegisterLabel16;
 
-    #[test]
-    fn ret_jumps_back_to_correct_place() {
-        // 0x00, 0x01, 0x02
-        let mut gb = Gameboy::new(vec![0xC9, 0x34, 0x12]); // RET
+#[test]
+fn ret_jumps_back_to_correct_place() {
+    // 0x00, 0x01, 0x02
+    let mut gb = Gameboy::new(vec![0xC9, 0x34, 0x12]); // RET
 
-        // 0x00 : (0xC9) The RET instruction
-        // 0x01 : (0x34) The lower byte of the return address
-        // 0x02 : (0x12) The higher byte of the return address
+    // 0x00 : (0xC9) The RET instruction
+    // 0x01 : (0x34) The lower byte of the return address
+    // 0x02 : (0x12) The higher byte of the return address
 
-        // The stack pointer points to 0x01 which means the value
-        // at the top of the stack is 0x1234
-        gb.set_register_16(RegisterLabel16::StackPointer, 0x01);
+    // The stack pointer points to 0x01 which means the value
+    // at the top of the stack is 0x1234
+    gb.set_register_16(RegisterLabel16::StackPointer, 0x01);
 
-        let cycles = gb.step_once();
+    let cycles = gb.step_once();
 
-        // RET takes 16 cycles
-        assert_eq!(cycles, 16);
+    // RET takes 16 cycles
+    assert_eq!(cycles, 16);
 
-        // Program counter is now in the correct place
-        assert_eq!(gb.get_register_16(RegisterLabel16::ProgramCounter), 0x1234);
+    // Program counter is now in the correct place
+    assert_eq!(gb.get_register_16(RegisterLabel16::ProgramCounter), 0x1234);
 
-        // Stack pointer is also in the right place
-        assert_eq!(gb.get_register_16(RegisterLabel16::StackPointer), 0x03);
-    }
+    // Stack pointer is also in the right place
+    assert_eq!(gb.get_register_16(RegisterLabel16::StackPointer), 0x03);
+}
 
-    #[test]
-    fn run_instruction_is_1_byte_big() {
-        // I'm not happy with this use of decode_instruction but
-        // I cannot see another way of checking the size of the RET
-        // instruction.
-        use crate::gameboy::opcodes::Decoder;
-        let instructions = vec![0xC9];
-        let opcode = Decoder::decode_instruction(0x00, &instructions).unwrap();
-        assert_eq!(opcode.size(), 1);
-    }
+#[test]
+fn run_instruction_is_1_byte_big() {
+    use crate::gameboy::opcodes::Decoder;
+    let instructions = vec![0xC9];
+    let opcode = Decoder::decode_instruction(0x00, &instructions).unwrap();
+    assert_eq!(opcode.size(), 1);
+}
+
+#[test]
+fn decode_ret_instructions() {
+    assert_eq!(
+        decode(&[0xC0]),
+        OpCode::new(
+            Category::RET,
+            [
+                Argument::JumpCondition(JumpCondition::NotZero),
+                Argument::None
+            ]
+        )
+    );
+}
+
+#[test]
+fn ret_can_have_nz_check() {
+    let opcode = OpCode::new(
+        Category::RET,
+        [
+            Argument::JumpCondition(JumpCondition::NotZero),
+            Argument::None,
+        ],
+    );
+
+    let mut memory = vec![0x00; 0xFF];
+    memory[0x01] = 0x34;
+    memory[0x02] = 0x12;
+
+    let mut cpu = CPU::new();
+
+    cpu.write_16_bits(RegisterLabel16::StackPointer, 0x01);
+    write_flag(&mut cpu, Flags::Z, true);
+
+    // Don't jump of zero flag is true
+    let cycles = opcode.run(&mut cpu, MemoryAdapter::new(&mut memory));
+    assert_eq!(cpu.read_16_bits(RegisterLabel16::ProgramCounter), 0x01);
+    assert_eq!(cycles, 8);
+
+    // Jump if zero flag is false
+    memory[0x01] = 0x34;
+    memory[0x02] = 0x12;
+    cpu.write_16_bits(RegisterLabel16::StackPointer, 0x01);
+    write_flag(&mut cpu, Flags::Z, false);
+
+    let cycles = opcode.run(&mut cpu, MemoryAdapter::new(&mut memory));
+    assert_eq!(cpu.read_16_bits(RegisterLabel16::ProgramCounter), 0x1234);
+    assert_eq!(cycles, 20);
+}
+
+#[test]
+fn ret_with_condition_still_1_byte() {
+    let opcode = OpCode::new(
+        Category::RET,
+        [
+            Argument::JumpCondition(JumpCondition::NotZero),
+            Argument::None,
+        ],
+    );
+
+    assert_eq!(opcode.size(), 1);
 }
