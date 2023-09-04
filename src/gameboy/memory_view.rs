522b0bc8d0e858e52b149147d616dc10d9d907d7
Guy Dunton <guydunton@economist.com>
2023-09-04 12:00:14 +0100

    Fix off by one errors in debugger (#34)

--- src/gameboy/memory_view.rs
+++ src/gameboy/memory_view.rs
@@ -13,7 +13,19 @@ impl<'a> MemoryView<'a> {
 
     pub fn get_memory_slice_at(&self, address: u16, size: u16) -> &'a [u8] {
         let start = address as usize;
-        let end = address.saturating_add(size) as usize;
+        let mut end = address as usize + size as usize;
+        if end > self.memory.len() {
+            end = self.memory.len();
+        }
         &self.memory[start..end]
     }
+}
+
+#[test]
+fn test_max_slice() {
+    let memory = vec![0x00; 0xFFFF + 1];
+    let view = MemoryView::new(&memory);
+
+    let slice = view.get_memory_slice_at(0xFFF0, 16);
+    assert_eq!(slice.len(), 16);
 }
