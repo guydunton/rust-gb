dfbe2803aadecde29f38dd0a78459f23dcb19a88
Guy Dunton <guydunton@economist.com>
2023-10-09 09:06:00 +0100

    Fix a CPL bug
    * This bug caused a code overflow running Tetris

--- src/gameboy/opcodes/run_fns/cpl.rs
+++ src/gameboy/opcodes/run_fns/cpl.rs
@@ -4,10 +4,9 @@ use super::super::Argument;
 
 #[allow(clippy::eq_op)]
 pub fn run_cpl(_: &[Argument], cpu: &mut CPU, _: &mut Vec<u8>) -> u32 {
-
     let a = cpu.read_8_bits(RegisterLabel8::A);
 
-    cpu.write_8_bits(RegisterLabel8::A, a^a);
+    cpu.write_8_bits(RegisterLabel8::A, !a);
 
     write_flag(cpu, Flags::N, true);
     write_flag(cpu, Flags::H, true);
